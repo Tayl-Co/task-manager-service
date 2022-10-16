3233f2a06ef32574b3321b24dba3ec9170cf2008
Rodrigo Limões <rolimoes@gmail.com>
2022-10-15 22:51:12 -0300

    Adding Project Entity

---
+++ src/project/entity/project.entity.ts
@@ -0,0 +1,31 @@
+import { Entity, Column, PrimaryColumn, ManyToOne, JoinTable } from 'typeorm';
+import { Field, ID, ObjectType } from '@nestjs/graphql';
+import { Team } from '@team/entity/team.entity';
+
+//TODO: Add property issues
+@ObjectType()
+@Entity()
+export class Project {
+    @PrimaryColumn({ unique: true, generated: true })
+    @Field(() => ID)
+    id: number;
+
+    @Field()
+    @Column()
+    name: string;
+
+    @Field()
+    @Column()
+    description: string;
+
+    @Field(() => Team)
+    @ManyToOne(() => Team, team => team.projects, {
+        cascade: ['insert', 'update', 'remove'],
+    })
+    @JoinTable()
+    team: Team;
+
+    @Field()
+    @Column()
+    active: boolean;
+}
