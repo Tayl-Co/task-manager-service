b8ca4e83b7b91a9b52b71b679354033d883d49bc
Rodrigo Limões <rolimoes@gmail.com>
2022-12-17 20:34:48 -0300

    Adding the create Query

--- src/label/label.resolver.ts
+++ src/label/label.resolver.ts
@@ -1,4 +1,14 @@
-import { Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Resolver } from '@nestjs/graphql';
+import { Label } from '@label/entity/label.entity';
+import { LabelDto } from '@label/dtos/label.dto';
+import { LabelService } from '@label/label.service';
 
 @Resolver()
-export class LabelResolver {}
+export class LabelResolver {
+    constructor(private labelService: LabelService) {}
+
+    @Mutation(() => Label, { name: 'createLabel' })
+    async create(@Args('labelInput') labelInput: LabelDto): Promise<Label> {
+        return await this.labelService.create(labelInput);
+    }
+}
--- src/label/label.service.ts
+++ src/label/label.service.ts
@@ -1,4 +1,13 @@
 import { Injectable } from '@nestjs/common';
+import { LabelRepository } from '@label/repository/label.repository';
+import { Label } from '@label/entity/label.entity';
+import { LabelDto } from '@label/dtos/label.dto';
 
 @Injectable()
-export class LabelService {}
+export class LabelService {
+    constructor(private labelRepository: LabelRepository) {}
+
+    create(labelInput: LabelDto): Promise<Label> {
+        return this.labelRepository.create(labelInput);
+    }
+}
