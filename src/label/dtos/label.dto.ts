596df258619cd4588afb379dc667bc55b70e2334
Rodrigo Limões <rolimoes@gmail.com>
2022-12-17 20:33:28 -0300

    Adding the Field decorator in Dto

--- src/label/dtos/label.dto.ts
+++ src/label/dtos/label.dto.ts
@@ -1,13 +1,15 @@
-import { InputType } from '@nestjs/graphql';
+import { Field, InputType } from '@nestjs/graphql';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 @InputType()
 export class LabelDto {
     @IsNotEmpty({ message: 'Required name' })
     @IsString()
+    @Field()
     name: string;
 
     @IsNotEmpty({ message: 'Required color' })
     @IsString()
+    @Field()
     color: string;
 }
