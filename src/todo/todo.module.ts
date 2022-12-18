5d33d82873e3c5d9c8a4c9475c4c15041888d43e
Rodrigo Limões <rolimoes@gmail.com>
2022-12-18 01:03:03 -0300

    Removing unnecessary repository class; Refactoring the ToDo service

--- src/todo/todo.module.ts
+++ src/todo/todo.module.ts
@@ -3,12 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ToDo } from '@todo/entity/todo.entity';
 import { TodoService } from './todo.service';
 import { TodoResolver } from './todo.resolver';
-import { ToDoRepository } from '@todo/repository/todo.repository';
 import { ProjectModule } from '@project/project.module';
 
 @Module({
     imports: [TypeOrmModule.forFeature([ToDo]), ProjectModule],
     exports: [TypeOrmModule, TodoService],
-    providers: [TodoService, TodoResolver, ToDoRepository],
+    providers: [TodoService, TodoResolver],
 })
 export class TodoModule {}
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,34 +1,61 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { ToDoRepository } from '@todo/repository/todo.repository';
 import { ToDo } from '@todo/entity/todo.entity';
 import { CreateToDoDto } from '@todo/dtos/createTodo.dto';
 import { ProjectService } from '@project/project.service';
 import { Project } from '@project/entity/project.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { IssueStatusEnum } from '@src/common/enums/issueStatus.enum';
+import { PriorityEnum } from '@src/common/enums/priority.enum';
 
 @Injectable()
 export class TodoService {
     constructor(
-        private todoRepository: ToDoRepository,
+        @InjectRepository(ToDo) private todoRepository: Repository<ToDo>,
         private projectService: ProjectService,
     ) {}
 
     async create(todoInput: CreateToDoDto): Promise<ToDo> {
-        const { projectId } = todoInput;
+        const {
+            title,
+            description,
+            assigneesIds,
+            parentId,
+            dueDate,
+            type,
+            projectId,
+        } = todoInput;
         let project: Project | null = null;
 
         if (projectId) {
             project = await this.projectService.findOne(projectId);
         }
 
-        return this.todoRepository.create(todoInput, project);
+        const todo = this.todoRepository.create({
+            title,
+            description,
+            assigneesIds,
+            project,
+            parentId,
+            dueDate: dueDate ? new Date(dueDate) : null,
+            type,
+            status: IssueStatusEnum.OPEN,
+            priority: PriorityEnum.LOW,
+            authorId: 'username', // TODO: Remove username mock
+        });
+
+        return this.todoRepository.save(todo);
     }
 
     async findAll(): Promise<Array<ToDo>> {
-        return await this.todoRepository.findAll();
+        return await this.todoRepository.find();
     }
 
     async findOne(id: number): Promise<ToDo> {
-        const todo = await this.todoRepository.findOne(id);
+        const todo = await this.todoRepository.findOne({
+            relations: { project: true, references: true },
+            where: { id },
+        });
 
         if (!todo) throw new NotFoundException(`ToDo ${id} not found`);
 
