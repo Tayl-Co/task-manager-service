4ff06ff2fa2b5dfa53bdd67ca9b3ff7ece0a04d9
Rodrigo Limões <rolimoes@gmail.com>
2022-12-18 00:21:11 -0300

    Removing unnecessary repository class;

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -3,11 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeamResolver } from '@team/team.resolver';
 import { Team } from '@team/entity/team.entity';
 import { TeamService } from '@team/team.service';
-import { TeamRepository } from '@team/repository/team.repository';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Team])],
-    providers: [TeamResolver, TeamService, TeamRepository],
+    providers: [TeamResolver, TeamService],
     exports: [TypeOrmModule, TeamService],
 })
 export class TeamModule {}
--- src/team/team.service.ts
+++ src/team/team.service.ts
@@ -1,46 +1,106 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
-import { TeamRepository } from '@team/repository/team.repository';
+import {
+    ConflictException,
+    Injectable,
+    NotFoundException,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import {
+    ArrayContains,
+    Equal,
+    FindOptionsOrderValue,
+    In,
+    Like,
+    Repository,
+} from 'typeorm';
 import { Team } from '@team/entity/team.entity';
 import { TeamDto } from '@team/dtos/team.dto';
 import { SearchFilterDto } from '@team/dtos/searchTeam.dto';
 
 @Injectable()
 export class TeamService {
-    constructor(private teamRepository: TeamRepository) {}
+    constructor(
+        @InjectRepository(Team)
+        private teamRepository: Repository<Team>,
+    ) {}
 
-    async create(team: TeamDto): Promise<Team> {
-        return await this.teamRepository.create(team);
+    async create(teamInput: TeamDto): Promise<Team> {
+        const team = await this.teamRepository.findOne({
+            where: { name: Equal(teamInput.name) },
+        });
+
+        if (team)
+            throw new ConflictException(`The ${team.name} Team already exists`);
+
+        const newTeam = this.teamRepository.create(teamInput);
+
+        return await this.teamRepository.save(newTeam);
     }
 
     async delete(id: number): Promise<Team> {
-        const team = await this.teamRepository.findOne(id);
+        const team = await this.findOne(id);
 
-        if (!team) throw new NotFoundException(`Team ${id} not found`);
+        await this.teamRepository.delete(id);
 
-        return await this.teamRepository.remove(team);
+        return team;
     }
 
-    async update(id: number, team: TeamDto): Promise<Team> {
-        const updateTeam = await this.teamRepository.findOne(id);
+    async update(id: number, teamInput: TeamDto): Promise<Team> {
+        const team = await this.findOne(id);
 
-        if (!updateTeam) throw new NotFoundException(`Team ${id} not found`);
+        Object.assign(team, teamInput);
 
-        return await this.teamRepository.update(id, team);
+        return await this.teamRepository.save(team);
     }
 
     async findAll(): Promise<Array<Team>> {
-        return await this.teamRepository.findAll();
+        return await this.teamRepository.find({
+            relations: { projects: true },
+            order: { name: 'ASC' as FindOptionsOrderValue },
+        });
     }
 
     async findOne(id: number): Promise<Team> {
-        const team = await this.teamRepository.findOne(id);
+        const team = await this.teamRepository.findOne({
+            relations: { projects: true },
+            where: { id: id },
+        });
 
         if (!team) throw new NotFoundException(`Team ${id} not found`);
 
         return team;
     }
 
-    async search(search: SearchFilterDto): Promise<Array<Team>> {
-        return await this.teamRepository.search(search);
+    async search(searchInput: SearchFilterDto): Promise<Array<Team>> {
+        const {
+            ids,
+            name,
+            ownerId,
+            membersIds,
+            managersIds,
+            page,
+            order,
+            limit,
+        } = searchInput;
+        let where = {};
+
+        if (ids) where = { ...where, id: In(ids) };
+
+        if (ownerId) where = { ...where, ownerId: ownerId };
+
+        if (membersIds)
+            where = { ...where, membersIds: ArrayContains(membersIds) };
+
+        if (managersIds)
+            where = { ...where, managersIds: ArrayContains(managersIds) };
+
+        return this.teamRepository.find({
+            relations: {
+                projects: true,
+            },
+            where: { name: Like(`%${name}%`), ...where },
+            order: { name: order as FindOptionsOrderValue },
+            take: limit,
+            skip: page * limit,
+        });
     }
 }
