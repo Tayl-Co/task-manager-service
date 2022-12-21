aa9a24cb084aae9acb23d32eaafca127a52bc19d
Rodrigo Limões <rolimoes@gmail.com>
2022-12-21 12:32:56 -0300

    E2E: Ensure save a team and return created Team

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,72 @@
-import { Test, TestingModule } from '@nestjs/testing'
-import { INestApplication } from '@nestjs/common'
-import * as request from 'supertest'
-import { AppModule } from '../src/app.module'
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from '@src/app.module';
+import request from 'supertest';
 
-describe('AppController (e2e)', () => {
-    let app: INestApplication
+const ENDPOINT = '/api/manager/task/';
+
+describe(ENDPOINT, () => {
+    let app: INestApplication;
 
     beforeEach(async () => {
         const moduleFixture: TestingModule = await Test.createTestingModule({
             imports: [AppModule],
-        }).compile()
+        }).compile();
 
-        app = moduleFixture.createNestApplication()
-        await app.init()
-    })
+        app = moduleFixture.createNestApplication();
 
-    it('/ (GET)', () => {
-        return request(app.getHttpServer())
-            .get('/')
-            .expect(200)
-            .expect('Hello World!')
-    })
-})
+        await app.init();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    describe('GRAPHQL MUTATION', () => {
+        describe('Create', () => {
+            it('createTeam - Should save a team and return created Team', () => {
+                return request(app.getHttpServer())
+                    .post(ENDPOINT)
+                    .send({
+                        query: `
+                        mutation{
+                            createTeam(teamInput: {
+                                name:"Team 1", 
+                                managersIds:["f522b8f6-3cf8-46cc-982f-b7017dc2c22c"], 
+                                membersIds: ["97e321ff-1a8b-4890-9cf2-2b05a5127267", "94e2b8ec-fdf3-4bb5-a6cc-cac47775b782"], 
+                                ownerId:"93a8a626-9938-40b5-9072-273cfc061c10"
+                                }){
+                                    id
+                                    name
+                                    managersIds
+                                    membersIds
+                                    ownerId
+                                    projects {
+                                      id
+                                    }
+                            }
+                        }
+                    `,
+                    })
+                    .expect(200)
+                    .expect({
+                        data: {
+                            createTeam: {
+                                id: '1',
+                                name: 'Team 1',
+                                managersIds: [
+                                    'f522b8f6-3cf8-46cc-982f-b7017dc2c22c',
+                                ],
+                                membersIds: [
+                                    '97e321ff-1a8b-4890-9cf2-2b05a5127267',
+                                    '94e2b8ec-fdf3-4bb5-a6cc-cac47775b782',
+                                ],
+                                ownerId: '93a8a626-9938-40b5-9072-273cfc061c10',
+                                projects: null,
+                            },
+                        },
+                    });
+            });
+        });
+    });
+});
