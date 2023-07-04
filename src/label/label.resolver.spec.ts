2622c41203df76b54daff533a0b04d3f458fffc5
Rodrigo Limões <rolimoes@gmail.com>
2023-07-04 20:27:49 -0300

    test: Ensure you research the labels.

--- src/label/label.resolver.spec.ts
+++ src/label/label.resolver.spec.ts
@@ -1,6 +1,7 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { LabelResolver } from './label.resolver';
 import { LabelService } from '@label/label.service';
+import { Order } from '@src/common/enums/order.enum';
 
 const label = { name: 'Feature', color: '#fff' };
 
@@ -31,36 +32,50 @@ describe('LabelResolver', () => {
 
     describe('Mutation', () => {
         const id = 1;
-        it('should create a label', async () => {
-            jest.spyOn(service, 'create').mockImplementation(() =>
-                Promise.resolve({ id: 1, ...label }),
-            );
 
-            await resolver.create({ name: 'Feature', color: '#fff' });
+        describe('createLabel', () => {
+            it('should create a label', async () => {
+                jest.spyOn(service, 'create').mockImplementation(() =>
+                    Promise.resolve({ id: 1, ...label }),
+                );
 
-            expect(service.create).toHaveBeenCalledTimes(1);
-            expect(service.create).toHaveBeenCalledWith(label);
+                const response = await resolver.create({
+                    name: 'Feature',
+                    color: '#fff',
+                });
+
+                expect(service.create).toHaveBeenCalledTimes(1);
+                expect(service.create).toHaveBeenCalledWith(label);
+                expect(response).toMatchObject({ id: 1, ...label });
+            });
         });
-        it('should delete a label', async () => {
-            jest.spyOn(service, 'delete').mockImplementation(() =>
-                Promise.resolve({ id, ...label }),
-            );
 
-            await resolver.delete(id);
+        describe('deleteLabel', () => {
+            it('should delete a label', async () => {
+                jest.spyOn(service, 'delete').mockImplementation(() =>
+                    Promise.resolve({ id, ...label }),
+                );
 
-            expect(service.delete).toHaveBeenCalledTimes(1);
-            expect(service.delete).toHaveBeenCalledWith(id);
+                const response = await resolver.delete(id);
+
+                expect(service.delete).toHaveBeenCalledTimes(1);
+                expect(service.delete).toHaveBeenCalledWith(id);
+                expect(response).toMatchObject({ id, ...label });
+            });
         });
-        it('should update a label', async () => {
-            const updateLabel = { ...label, name: 'Feature2' };
-            jest.spyOn(service, 'update').mockImplementation(() =>
-                Promise.resolve({ id, ...updateLabel }),
-            );
+        describe('updateLabel', () => {
+            it('should update a label', async () => {
+                const updateLabel = { ...label, name: 'Feature2' };
+                jest.spyOn(service, 'update').mockImplementation(() =>
+                    Promise.resolve({ id, ...updateLabel }),
+                );
 
-            await resolver.update(id, updateLabel);
+                const response = await resolver.update(id, updateLabel);
 
-            expect(service.update).toHaveBeenCalledTimes(1);
-            expect(service.update).toHaveBeenCalledWith(id, updateLabel);
+                expect(service.update).toHaveBeenCalledTimes(1);
+                expect(service.update).toHaveBeenCalledWith(id, updateLabel);
+                expect(response).toMatchObject({ id, ...updateLabel });
+            });
         });
     });
 
@@ -69,10 +84,34 @@ describe('LabelResolver', () => {
 
         describe('findOneLabel', () => {
             it('should return a label', async () => {
-                await resolver.findOne(id);
+                jest.spyOn(service, 'findOne').mockImplementation(() =>
+                    Promise.resolve({ id, ...label }),
+                );
+                const response = await resolver.findOne(id);
 
                 expect(service.findOne).toHaveBeenCalledTimes(1);
                 expect(service.findOne).toHaveBeenCalledWith(2);
+                expect(response).toMatchObject({ id, ...label });
+            });
+        });
+
+        describe('searchLabel', () => {
+            it('should search labels', async () => {
+                jest.spyOn(service, 'search').mockImplementation(() =>
+                    Promise.resolve([{ id, ...label }]),
+                );
+                const searchInput = {
+                    name: 'feat',
+                    page: 0,
+                    limit: 10,
+                    orderBy: 'name',
+                    sortOrder: Order.ASC,
+                };
+                const response = await resolver.search(searchInput);
+
+                expect(service.search).toHaveBeenCalledTimes(1);
+                expect(service.search).toHaveBeenCalledWith(searchInput);
+                expect(response).toMatchObject([{ id, ...label }]);
             });
         });
     });
