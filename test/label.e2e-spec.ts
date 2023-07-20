f1b4275412f3c7c372ae2abe5ea8dbb4189a466c
Rodrigo Limões <rolimoes@gmail.com>
2023-07-20 17:28:40 -0300

    test: Ensure find the label

--- test/label.e2e-spec.ts
+++ test/label.e2e-spec.ts
@@ -147,4 +147,44 @@ describe('Label e2e Test', () => {
                 data: null,
             });
     });
+
+    it('should find the label', async () => {
+        const {
+            body: {
+                data: {
+                    createLabel: { ...label },
+                },
+            },
+        } = await request(httpServer)
+            .post(ENDPOINT)
+            .send({
+                query: `mutation{
+                            createLabel(
+                                labelInput:{
+                                    name:"Label 1", 
+                                    color: "#FBCA04"
+                                    })
+                                {
+                                    id
+                                    name
+                                    color
+                                }
+                        }`,
+            })
+            .expect(HttpStatus.OK);
+
+        return request(httpServer)
+            .post(ENDPOINT)
+            .send({
+                query: `query{
+                    findOneLabel(id:${label.id}){
+                        id
+                        name
+                        color
+                    }
+                }`,
+            })
+            .expect(HttpStatus.OK)
+            .expect({ data: { findOneLabel: label } });
+    });
 });
