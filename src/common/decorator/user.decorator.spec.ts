8ccd5f59dc2479d4b2c333d3102d5b82c53f7f85
Rodrigo Limões <rolimoes@gmail.com>
2023-07-28 21:44:00 -0300

    test: Ensure return userid if id exists

--- src/common/decorator/user.decorator.spec.ts
+++ src/common/decorator/user.decorator.spec.ts
@@ -9,10 +9,29 @@ const ctxMock: any = {
 };
 
 describe('User decorator', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
     it('should return an unknown value if user not exists', () => {
         const user = getUser('id', ctxMock);
 
         expect(user).toBeDefined();
         expect(user).toEqual('unknown');
+    });
+
+    it('should return userid if id exists', async () => {
+        jest.spyOn(ctxMock, 'switchToHttp').mockImplementation(() => ({
+            getRequest: (): any => ({
+                user: {
+                    id: '08b8b93a-9aa7-4fc1-8201-539e2cb33830',
+                },
+            }),
+        }));
+
+        const userId = getUser('id', ctxMock);
+
+        expect(userId).toBeDefined();
+        expect(userId).toEqual('08b8b93a-9aa7-4fc1-8201-539e2cb33830');
     });
 });
